cf31dd0d7de820f080598082673aede62b950a34
Rajayne <ryann.j.nealey@gmail.com>
2023-03-17 14:57:30 -0700

    Create users table

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash
+from models import db, connect_db, User
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.app_context().push()
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def list_users():
+    users = User.query.all()
+    return render_template('list.html', users=users)
--- models.py
+++ models.py
@@ -5,3 +5,24 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+"""Models for Blogly."""
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String(15),
+                           nullable=False,
+                           required=True)
+    
+    last_name = db.Column(db.String(15),
+                          nullable=False,
+                          required=True)
+    
+    image_url = db.Column(db.String(256),
+                          nullable=False
+                          default='')
+    
