2bc1df2fee4cab8db9d2fe63994e244f3525d5bc
Rajayne <ryann.j.nealey@gmail.com>
2023-03-17 15:11:06 -0700

    Create required routes

--- app.py
+++ app.py
@@ -14,6 +14,28 @@ connect_db(app)
 db.create_all()
 
 @app.route('/')
+def show_users():
+    return redirect('/users')
+
+@app.route('/users')
 def list_users():
     users = User.query.all()
     return render_template('list.html', users=users)
+
+# @app.route('/users/new', method=['GET'])
+# def new_user_form():
+
+# @app.route('/users/new', method=['POST'])
+# def add_new_user():
+
+# @app.route('/users/<int:user_id>')
+# def view_user():
+
+# @app.route('/users/<int:user_id>/edit', method=['GET'])
+# def edit_user_form():
+
+# @app.route('/users/<int:user_id>/edit', method=['POST'])
+# def submit_user_edit():
+
+# @app.route('/users/<int:user_id>/delete')
+# def delete_user():
